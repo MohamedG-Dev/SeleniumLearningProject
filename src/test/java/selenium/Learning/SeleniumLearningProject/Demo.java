0135ef7fe82b37882cc9fcef490bbde795198a53
G <mohamedosama.g@atos.net>
2022-02-08 09:31:37 +0530

    selenium maven project

---
+++ src/test/java/selenium/Learning/SeleniumLearningProject/Demo.java
@@ -0,0 +1,21 @@
+package selenium.Learning.SeleniumLearningProject;
+
+import java.util.logging.Logger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void LoginIn() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://omayo.blogspot.com/");
+		driver.findElement(By.name("q")).sendKeys("XYZ MOTORS");
+		
+	}
+}
